69df65638b69230200ce74350a3e012efa4da236
Martynas Baltutis <martynas.baltutis@gmail.com>
2021-02-23 23:48:45 +0200

    Add responsiveness and twitter icon

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React, { useState, useEffect } from "react";
 import "./index.css";
+import { FaTwitterSquare } from "react-icons/fa";
 
 const url = "https://type.fit/api/quotes";
+
+// Work more on landscape view
+// Inspect tweeting
 
 function App() {
   const [quotes, setQuotes] = useState([]);
@@ -17,16 +21,14 @@ function App() {
     const response = await fetch(url);
     const newQuotes = await response.json();
     setQuotes(newQuotes);
-    setNumber(Math.floor(Math.random() * newQuotes.length))
-    setLoading(false);    
+    setNumber(Math.floor(Math.random() * newQuotes.length));
+    setLoading(false);
   };
-
-
 
   const randomQuote = () => {
     let randomNumber = Math.floor(Math.random() * quotes.length);
 
-    if (randomNumber === number && randomNumber !==  quotes.length) {
+    if (randomNumber === number && randomNumber !== quotes.length) {
       randomNumber = number + 1;
     }
     setNumber(randomNumber);
@@ -37,12 +39,9 @@ function App() {
     fetchQuotes();
   }, []);
 
-
-
   if (loading) {
     return <h2 className="text-center">Loading...</h2>;
   }
-
 
   return (
     <div className="mx-auto" id="quote-box">
@@ -56,15 +55,27 @@ function App() {
           {quotes[number].author}
         </h4>
       )}
-      <button
-        className="btn btn-light"
-        id="new-quote"
-        onClick={() => {
-          fadeout(); setTimeout(randomQuote, 1000);
-        }}
-      >
-        New quote
-      </button>
+      <div className="btn-group" role="group">
+        <a
+          href="https://twitter.com/share?ref_src=twsrc%5Etfw"
+          id="twitter-share-button"
+          className="btn"
+          data-show-count="false"
+          target="_blank"
+        >
+          <FaTwitterSquare size="3rem" color="black" />
+        </a>
+        <button
+          className="btn btn-light"
+          id="new-quote"
+          onClick={() => {
+            fadeout();
+            setTimeout(randomQuote, 1000);
+          }}
+        >
+          New quote
+        </button>
+      </div>
     </div>
   );
 }
